451eade40f397e4dc8b9684ddfc91b968d43b7ae
Mahapara Naim <mahaparanaim57@gmail.com>
2023-07-18 13:39:07 +0600

    updated-styling of form

--- components/EmptyLink.js
+++ components/EmptyLink.js
@@ -2,7 +2,7 @@ const EmptyLink = () => {
   return (
     <div>
       {/* <LinkContainer /> */}
-      <h1 className="text-blueGray-500 uppercase text-2xl text-center font-serif font-medium p-12 m-12">
+      <h1 className="text-blueGray-500 uppercase text-2xl text-center font-sans font-medium p-12 m-12">
         No links yet!
       </h1>
     </div>
